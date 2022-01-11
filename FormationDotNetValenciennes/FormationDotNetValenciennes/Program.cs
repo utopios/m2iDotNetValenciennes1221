b77f239d1383ed592b00f0970c1f06487042802e
ihab abadi <ihab@utopios.net>
2022-01-11 14:35:03 +0100

    correction ex1

--- FormationDotNetValenciennes/FormationDotNetValenciennes/Program.cs
+++ FormationDotNetValenciennes/FormationDotNetValenciennes/Program.cs
@@ -1,2 +1,17 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+//Console.WriteLine("Hello, World!");
+
+
+using System;
+namespace FormationDotNetValenciennes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            //Console.WriteLine("Bonjour tout le monde");
+        }
+    }
+}
+
