68e90b1199f1b282dc504b6b8f9eccf07be0734e
ihab abadi <ihab@utopios.net>
2022-01-11 16:18:35 +0100

    correction exercice 2 et 3

---
+++ CorrectionExercices/CorrectionExercice2Et3/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace CorrectionExercice2Et3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Veuillez saisir votre nom : ");
+            string nom = Console.ReadLine();
+            Console.Write("Veuillez saisir votre prénom : ");
+            string prenom = Console.ReadLine();
+            Console.WriteLine("Bonjour "+nom +" "+prenom);
+        }
+    }
+}
