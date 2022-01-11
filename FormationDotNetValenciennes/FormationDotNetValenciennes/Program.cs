e2f2a2b38482ea6885fe26c06d134f2c4070e3e1
ihab abadi <ihab@utopios.net>
2022-01-11 15:22:25 +0100

    suite cours c# console readline et variable string

--- FormationDotNetValenciennes/FormationDotNetValenciennes/Program.cs
+++ FormationDotNetValenciennes/FormationDotNetValenciennes/Program.cs
@@ -9,8 +9,24 @@ namespace FormationDotNetValenciennes
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Console.Write("Bonjour ");
+            //Console.Write("toto");
+            //Console.WriteLine("Hello World!");
+            //Console.Write("All");
             //Console.WriteLine("Bonjour tout le monde");
+            //string result;
+            //Console.Write("Saisir un element : ");
+            //string result = Console.ReadLine();
+            ////Console.Read();
+            //Console.WriteLine(result);
+
+            //Déclarer une variable pour stocker le nom
+            //CamelCase => façon de nommer les variables
+            string nomComplet = "abadi ihab";
+            Console.WriteLine(nomComplet);
+            //nomComplet = @"M. Abadi ihab \\ Tourcoing";
+            nomComplet = "M." + nomComplet;
+            Console.WriteLine(nomComplet);
         }
     }
 }
