47a735251e2a34c338c49a2f9f44cb867ebe9b2b
ihab abadi <ihab@utopios.net>
2022-02-03 11:44:43 +0100

    correction pendu

--- CorrectionExercicesPOO/CorrectionPendu/Program.cs
+++ CorrectionExercicesPOO/CorrectionPendu/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CorrectionPendu.Classes;
+using System;
 
 namespace CorrectionPendu
 {
@@ -6,7 +7,7 @@ namespace CorrectionPendu
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            new IHM().Start();
         }
     }
 }
