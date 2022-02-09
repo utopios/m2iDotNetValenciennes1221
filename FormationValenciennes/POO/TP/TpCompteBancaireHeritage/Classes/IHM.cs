029d1f9508e50b83d59366ccf85ed7916f342165
PetitVulcan <dipersio.a@gmail.com>
2022-02-09 14:43:48 +0100

    Fin correction TpBanqueHeritage

--- FormationValenciennes/POO/TP/TpCompteBancaireHeritage/Classes/IHM.cs
+++ FormationValenciennes/POO/TP/TpCompteBancaireHeritage/Classes/IHM.cs
@@ -19,10 +19,13 @@ namespace TpCompteBancaireHeritage.Classes
 
         public void Start()
         {
-            Menu();
-            string choix = Console.ReadLine();
+           
+            string choix;
+            
             do
             {
+                Console.Clear();
+                choix = Menu();
                 switch (choix)
                 {
                     case "1":
@@ -40,11 +43,15 @@ namespace TpCompteBancaireHeritage.Classes
                     case "5":
                         ActionCalculInterets();
                         break;
+                    case "0":
+                        Environment.Exit(0);
+                        break;
                 }
-            } while (true);
+
+            } while (choix != "0");
         }
 
-        private void Menu()
+        private string Menu()
         {
             Console.WriteLine("\n--------------- Banque Peu Populaire -------------\n");
             Console.WriteLine("1- Créer un compte bancaire");
@@ -55,60 +62,142 @@ namespace TpCompteBancaireHeritage.Classes
             Console.WriteLine("0---Quitter\n");
 
             Console.Write("Faites votre choix : ");
+
+            return Console.ReadLine();
+        }
+        private string MenuCreationCompte()
+        {
+            Console.WriteLine("\n----- Type de compte -----\n");
+            Console.WriteLine("1- Compte Normal");
+            Console.WriteLine("2- Compte Epargne");
+            Console.WriteLine("3- Compte Payant");
+
+            Console.Write("\nFaites votre choix : ");
+            return Console.ReadLine();
         }
 
-        public void ActionCreationCompte()
+        public void CreationClient(Client c)
         {
+            Console.WriteLine("----- Création d'un client ------\n");
+            Console.Write("Veuillez saisir un nom : ");
+            c.Nom = Console.ReadLine();
+            Console.Write("Veuillez saisir un prénom : ");
+            c.Prenom = Console.ReadLine();
+            Console.Write("Veuillez saisir le téléphone : ");
+            c.Telephone = Console.ReadLine();            
+        }
+        public double CreationCompte()
+        {
+            Console.WriteLine("----- Création du compte ------\n");
+            Console.Write("Veuillez saisir le solde à l'ouverture du compte : ");
+            return Convert.ToDouble( Console.ReadLine() );           
+        }
+        public void ActionCreationCompte()
+        {            
+            // Création d'un client
             Client client = new Client();
+            CreationClient(client);
 
-            Compte compte = new Compte();   
+            // Création du compte           
+            double solde = CreationCompte();
+            
+            string choix = MenuCreationCompte();
 
-            compte.AjouterCompte(bank);
-            // Création du compte (Compte, CompteEpargne,ComptePayant)
+            Compte compte = null;
+            switch (choix)
+            {
+                case "1":
+                    compte = new Compte(solde,client);
+                    break;
+                case "2":
+                    Console.Write("Veuillez saisir le taux de rémunération : ");
+                    double taux = Convert.ToDouble(Console.ReadLine());
+                    // Création d'un compte Epargne
+                    compte = new CompteEpargne(solde, client, taux );
+                    break;
+                case "3":
+                    Console.Write("Veuillez saisir le coût d'une opération : ");
+                    double cout = Convert.ToDouble(Console.ReadLine());
+                    // Création d'un compte Payant
+                    compte = new ComptePayant(solde, client, cout);
+                    break;
+                default:
+                    Console.WriteLine("Erreur de choix");
+                    break;
+            }
+
+            if (compte != null)
+            {
+                compte.AjouterCompte(bank);
+            }
+            else
+            {
+                Console.WriteLine("Erreur lors de la création du compte");
+            }            
+            
         }
 
         public void ActionDepot()
         {
-            // Collecter les éléments pour créer un dépot:
-            // Trouver le compte
+            Console.Write("---------- Déposer des fonds ----------\n");
+            Compte c = ActionRechercherCompte();
+            Console.Write("Veuillez saisir le montant du dépot : ");
+            double montant = Convert.ToDouble(Console.ReadLine());
+
             // Montant => créer l'opération
-            // Ajouter l'opération a la liste des opération du compte
+            Operation o = new Operation(montant);
+
             // Update le solde du compte
+            c.Depot(o);
         }
 
         public void ActionRetrait()
         {
-            // Collecter les éléments pour créer un retrait:
-            // Trouver le compte
+            Console.Write("---------- Retirer des fonds ----------\n");
+            Compte c = ActionRechercherCompte();
+
+            Console.Write("Veuillez saisir le montant du retrait : ");
+            double montant = Convert.ToDouble(Console.ReadLine())*-1;
+
             // Montant => créer l'opération
-            // Ajouter l'opération a la liste des opération du compte
-            // Update le solde du compte
+            Operation o = new Operation(montant);
+
+            // Ajout du retrait au compte + update Operation et solde
+            c.Retrait(o);
         }
 
         public void ActionAffichageCompte()
         {
+            Console.Write("---------- Afficher un compte ----------\n");
+
             Compte c = null;
             c = ActionRechercherCompte();
-            Console.WriteLine(c);
+           
             if(c != null)
             {
-                // Recupérer l'id au client (fait)
-                // Rechercher le compte avec l'id (fait)
-                // Afficher le compte (fait)
+                Console.WriteLine(c);
             }
             else            
-                Console.WriteLine("Erreur, veuillez choisir un compte valable...");           
+                Console.WriteLine("Erreur, veuillez choisir un compte valable...");
+            WaitUser();
         }
 
         public void ActionCalculInterets()
         {
             Compte c = ActionRechercherCompte();
-            // Recupérer l'id au client
-            // Rechercher le compte avec l'id
-            // Vérifier qu'il s'agit d'un compte épargne ( typeof() )
-            // Calculer les intérêts
-            // Ajouter l'opération
-            // Update le solde
+            if (c !=null && c is CompteEpargne compteEpargne)
+            {
+                if (compteEpargne.CalculerInteret())
+                {
+                    Console.WriteLine("Intérêts ajoutés !");
+                    WaitUser();
+                }
+            }
+            else
+            {
+                Console.WriteLine("Veuillez séléctionner un compte Epargne!");
+                WaitUser();
+            }
         }
 
         public Compte ActionRechercherCompte()
@@ -116,6 +205,13 @@ namespace TpCompteBancaireHeritage.Classes
             Console.Write("Veuillez saisir l'id du compte : ");
             int id = Convert.ToInt32(Console.ReadLine());
             return Compte.RechercherCompte(bank, id);
+        }
+
+        private void WaitUser()
+        {
+            Console.WriteLine("Veuillez presser Enter pour retourner au menu principal");
+            Console.ReadLine();
+            Console.Clear();
         }
     }
 }
