e57405669c0bda59dcd862347881a242ed50e2fa
PetitVulcan <dipersio.a@gmail.com>
2022-02-09 16:59:35 +0100

    Cours Ado.NET Part 2

--- FormationValenciennes/CoursAdoNET/Program.cs
+++ FormationValenciennes/CoursAdoNET/Program.cs
@@ -18,8 +18,8 @@ namespace CoursAdoNET
             // string request = "INSERT INTO UTILISATEUR (nom, prenom,email,telephone) VALUES ('Toto','Titi','toto@gmail.com','+33 6 41 52 63 78') ";
             // string request2 = "INSERT INTO UTILISATEUR (nom, prenom,email,telephone) OUTPUT INSERTED.Id VALUES ('Duss','Jean-Claude','jcd@gmail.com','+33 6 74 21 85 74') ";
             //string request3 = "SELECT id, nom, prenom,email,telephone FROM UTILISATEUR";
-            
-            
+
+
             //SqlCommand command = new SqlCommand(request3, connection);
 
             //// Ouverture de la connection
@@ -50,11 +50,47 @@ namespace CoursAdoNET
             //// Fermeture de la connection à la BDD
             //connection.Close(); 
 
+            ///
+            /// Avec récupération des saisies utilisateur => Parametres (SqlParameters)
+            ///
+
+            // Executer les requêtes avec des variables => On utilise pour cela une requête préparée avec des paramètres
+            Console.Write("Merci de saisir votre nom : ");
+            string nom = Console.ReadLine();
+            Console.Write("Merci de saisir votre prénom : ");
+            string prenom = Console.ReadLine();
+            Console.Write("Merci de saisir votre email : ");
+            string email = Console.ReadLine();
+            Console.Write("Merci de saisir votre téléphone : ");
+            string telephone = Console.ReadLine();
+
+            // Préparation de la commande
+            string request = $"INSERT INTO UTILISATEUR (nom, prenom, email, telephone) VALUES (@nom, @prenom, @email, @telephone)";
+            //string request = $"INSERT INTO UTILISATEUR (nom, prenom, email, telephone) OUTPUT INSERTED.Id VALUES (@nom, @prenom, @email, @telephone)";
+            SqlCommand command = new SqlCommand(request, connection);
+            // Ajout de parametres 
+            command.Parameters.Add(new SqlParameter("@nom", nom));
+            command.Parameters.Add(new SqlParameter("@prenom", prenom));
+            command.Parameters.Add(new SqlParameter("@email", email));
+            command.Parameters.Add(new SqlParameter("@telephone", telephone));
+
+            // Ouverture de la connection
+            connection.Open();
+
+            // 1ere méthode d'execution d'une commande => Retourne le nombre de lignes affectées par la commande 
+            int nbLigne = command.ExecuteNonQuery();
+            Console.WriteLine("Nombre de lignes affectées : " + nbLigne);
+
+            //// 2eme méthode d'execution d'une commande => Retourne le nombre champ indiqué dans la requete 
+            //int Id = (int)command.ExecuteScalar();
+            //Console.WriteLine("Id d'insertion dans la table : " + Id);
 
 
+            // Liberation de l'objet command
+            command.Dispose();
 
-
-
+            // Fermeture de la connection à la BDD
+            connection.Close();
             #endregion
 
 
