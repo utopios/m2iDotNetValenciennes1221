124c204a625f735ac150b27b5610dea251c8b2ed
ihab abadi <ihab@utopios.net>
2022-02-10 11:45:26 +0100

    debut correction annuaire

--- FormationValenciennes/CoursAdoNET/Program.cs
+++ FormationValenciennes/CoursAdoNET/Program.cs
@@ -11,19 +11,20 @@ namespace CoursAdoNET
             // Connection à la BDD
             string connectionString = @"Data Source=(LocalDB)\M2iValenciennes;Integrated Security=True";
 
+            //La création d'un objet pour établir une connexion vers notre base de données
             SqlConnection connection = new SqlConnection(connectionString);
 
             // Pour executer une requete SQl dans un programme Ado.NET => Utilisation d'un objet command
             // Préparation de la commande
-            // string request = "INSERT INTO UTILISATEUR (nom, prenom,email,telephone) VALUES ('Toto','Titi','toto@gmail.com','+33 6 41 52 63 78') ";
-            // string request2 = "INSERT INTO UTILISATEUR (nom, prenom,email,telephone) OUTPUT INSERTED.Id VALUES ('Duss','Jean-Claude','jcd@gmail.com','+33 6 74 21 85 74') ";
-            //string request3 = "SELECT id, nom, prenom,email,telephone FROM UTILISATEUR";
+            //string request = "INSERT INTO UTILISATEUR (nom, prenom,email,telephone) VALUES ('Toto','Titi','toto@gmail.com','+33 6 41 52 63 78') ";
+            //string request2 = "INSERT INTO UTILISATEUR (nom, prenom,email,telephone) OUTPUT INSERTED.Id VALUES ('Duss','Jean-Claude','jcd@gmail.com','+33 6 74 21 85 74') ";
+            string request3 = "SELECT id, nom, prenom,email,telephone FROM UTILISATEUR";
 
 
-            //SqlCommand command = new SqlCommand(request3, connection);
+            SqlCommand command = new SqlCommand(request3, connection);
 
             //// Ouverture de la connection
-            //connection.Open();
+            connection.Open();
 
             //// 1ere méthode d'execution d'une commande => Retourne le nombre de lignes affectées par la commande 
             //int nbLigne = command.ExecuteNonQuery();
@@ -34,28 +35,28 @@ namespace CoursAdoNET
             //Console.WriteLine("Id d'insertion dans la table : " + Id);
 
             //// 3eme méthode d'execution de la commande => Avec un reader
-            //SqlDataReader reader = command.ExecuteReader();
+            SqlDataReader reader = command.ExecuteReader();
 
-            //while (reader.Read())
-            //{
-            //    Console.WriteLine($"Id : {reader.GetInt32(0)}, Nom : {reader.GetString(1)}, Prénom : {reader.GetString(2)}, Email : {reader.GetString(3)},  Téléphone : {reader.GetString(4)}");
-            //}
+            while (reader.Read())
+            {
+                Console.WriteLine($"Id : {reader.GetInt32(0)}, Nom : {reader.GetString(1)}, Prénom : {reader.GetString(2)}, Email : {reader.GetString(3)},  Téléphone : {reader.GetString(4)}");
+            }
 
             //// Libération du reader
-            //reader.Close();
+            reader.Close();
 
             //// Liberation de l'objet command
-            //command.Dispose();
+            command.Dispose();
 
             //// Fermeture de la connection à la BDD
-            //connection.Close(); 
+            connection.Close(); 
 
             ///
             /// Avec récupération des saisies utilisateur => Parametres (SqlParameters)
             ///
 
             // Executer les requêtes avec des variables => On utilise pour cela une requête préparée avec des paramètres
-            Console.Write("Merci de saisir votre nom : ");
+            /*Console.Write("Merci de saisir votre nom : ");
             string nom = Console.ReadLine();
             Console.Write("Merci de saisir votre prénom : ");
             string prenom = Console.ReadLine();
@@ -103,7 +104,8 @@ namespace CoursAdoNET
 
 
             Console.WriteLine("Appuyez sur ENTER pour fermer le programme...");
-            Console.Read();
+            Console.Read();*/
+            #endregion
         }
     }
 }
