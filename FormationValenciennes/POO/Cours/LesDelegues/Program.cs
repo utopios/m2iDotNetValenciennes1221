0e2f2f9263d6bb5f73df8717c3747b18a80d4131
Anthony Di Persio <dipersio.a@gmail.com>
2022-03-28 11:31:58 +0200

    Ajout Cours PassageParams

---
+++ FormationValenciennes/POO/Cours/LesDelegues/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace LesDelegues
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Appuyez sur ENTER pour fermer le programme...");
+            Console.Read();
+        }
+    }
+}
---
+++ FormationValenciennes/POO/Cours/PassageParams/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace PassageParams
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * Le Passage d'arguments peut se faire par valeur ( pour les variabless de type valeur => Copie de la valeur)
+             * Le Passage d'arguments peut se faire par référence ( pour les variables de type objet => Copie de la valeur)
+             * Il possible de modifier ce comportement avec l'ajout de mot clé => IN , REF, OUT
+             * IN => Pour passer un paramètre par référence avec l’intention d’éviter la copie, mais de ne pas changer la valeur.
+             * REF=> Pour passer une valeur par référence => Afin de modifier sa valeur
+             * OUT=> Pour Ecrire la valur en sortie de la fonction 
+             */
+
+
+            // OUT 
+            //string ageStr= "20";
+            int age;
+            Console.Write("Veuillez saisir votre age :");
+
+            //while(int.TryParse(ageStr, out age))
+            while(!int.TryParse(Console.ReadLine(), out age))        
+            {
+                Console.Write("Erreur, Veuillez saisir votre age :");
+            }
+
+            Console.WriteLine($"Vous avez {age} ans.");
+
+
+
+
+
+
+            Console.WriteLine("Appuyez sur ENTER pour fermer le programme...");
+            Console.Read();
+
+        }
+    }
+}
