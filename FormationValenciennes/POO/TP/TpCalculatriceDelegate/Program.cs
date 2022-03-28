be1a8abea68024c1189ced0debbcc2c0c2ae2a98
Anthony Di Persio <dipersio.a@gmail.com>
2022-03-28 12:15:02 +0200

    Ajout Sujet Tp Delegate

---
+++ FormationValenciennes/POO/TP/TpCalculatriceDelegate/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace TpCalculatriceDelegate
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        { 
+            /*
+             * Créer un programme console demandant la saisie de deux nombre à l'utilisateur et une fois fait
+             * proposer un menu avec les 4 types d'opérations (+, - , / , *)
+             * Afficher le résultat
+             */
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
