c85a03f2fc4d1c7fee9ba340620cb026516ae3ff
PetitVulcan <dipersio.a@gmail.com>
2022-04-04 17:15:28 +0200

    Add Correction ProverbApi

---
+++ FormationValenciennes/AspNET/ApiProverbsEntity/Models/Proverb.cs
@@ -0,0 +1,15 @@
+﻿namespace ApiProverbsEntity.Models
+{
+    public class Proverb
+    {
+        private int id;
+        private string author;
+        private string saying;
+        private string language;
+
+        public int Id { get => id; set => id = value; }
+        public string Author { get => author; set => author = value; }
+        public string Saying { get => saying; set => saying = value; }
+        public string Language { get => language; set => language = value; }
+    }
+}
---
+++ FormationValenciennes/AspNET/ApiProverbsEntity/Tools/DataDbContext.cs
@@ -0,0 +1,20 @@
+﻿using ApiProverbsEntity.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApiProverbsEntity.Tools
+{
+    public class DataDbContext : DbContext
+    {
+        public DataDbContext()
+        {
+              
+        }
+
+        public DbSet<Proverb> Proverbs { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Data Source=(localdb)\ApiProverbs;Integrated Security=True");
+        }
+    }
+}
