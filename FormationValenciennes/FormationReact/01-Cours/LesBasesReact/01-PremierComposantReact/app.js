984ce269af01cb7bf2f2813ebc2035a68c358e91
PetitVulcan <dipersio.a@gmail.com>
2022-04-11 14:22:23 +0200

    Ajout Cours premier composant react

--- FormationValenciennes/FormationReact/01-Cours/LesBasesReact/01-PremierComposantReact/app.js
+++ FormationValenciennes/FormationReact/01-Cours/LesBasesReact/01-PremierComposantReact/app.js
@@ -8,15 +8,47 @@ const elementHTML = <h2>Bonjour, {nom}</h2>;
 /**
  * 1 - Creation d'un composant simple (component)
  */
-function MyComposant(){
-    return(
-        <h2>Bonjour, {nom}</h2>
+// function MyComposant(){
+//     return(
+//         <h2>Bonjour, {nom}</h2>
+//     )
+// }
+
+
+/**
+ * 2 - Creation d'un composant avec Params (component)
+ */
+// function MyComposant({nom, prenom}){
+//     return(
+//         <h2>Bonjour, {nom} {prenom}</h2>
+//     )
+// }
+
+/**
+ * 3 - Creation d'un composant avec Params (component)
+ */
+const user={
+    firstname:"Anthony",
+    lastname:"Di Persio"
+}
+
+function formatName(user){
+    // console.log("FormatName : "+user);
+    return user.firstname+" "+user.lastname;    
+}
+
+ function MyComposant({user}){
+    return(        
+        <h2>Bonjour, {formatName(user)}</h2>
     )
 }
 
-
-
-
+// Avec Props
+// function MyComposant(props){
+//     return(        
+//         <h2>Bonjour, {formatName(props.user)}</h2>
+//     )
+// }
 
 
 /**
@@ -30,4 +62,11 @@ ReactDOM.render(
 /**
  * 1- Rendu => Avec un composant
  */
-ReactDOM.render(<MyComposant/>,document.getElementById('app') )
+// // 1- Rendu ReactDOM.Render() sans params
+// ReactDOM.render(<MyComposant/>,document.getElementById('app') )
+
+// // 2 - Rendu ReactDOM.Render() avec params
+// ReactDOM.render(<MyComposant nom="Di Persio" prenom="Anthony"/>,document.getElementById('app') )
+
+// 3 - Rendu ReactDOM.Render() avec params
+ReactDOM.render(<MyComposant user={user}/>,document.getElementById('app') )
