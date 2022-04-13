2f24ea9f31ab7e9f860c8d997a57b2bedbb7ff7f
PetitVulcan <dipersio.a@gmail.com>
2022-04-13 17:11:39 +0200

    Ajout local storage

--- FormationValenciennes/FormationReact/01-Cours/LesBasesReact/04-ProjetStateLess/src/App.js
+++ FormationValenciennes/FormationReact/01-Cours/LesBasesReact/04-ProjetStateLess/src/App.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import Banner from './components/BannerComponent/Banner';
 import NavBar from './components/NavBarComponent/NavBar';
 import Footer from './components/FooterComponent/Footer';
@@ -8,7 +8,14 @@ import './App.css';
 
 
 function App() {
-  const [cart,updateCart] = useState([]);
+  //const [cart,updateCart] = useState([]);
+  const savedCard = localStorage.getItem('cart')
+
+  const [cart,updateCart] = useState(savedCard?JSON.parse(savedCard):[]);
+
+  useEffect(()=>{
+    localStorage.setItem('cart', JSON.stringify(cart))
+  },[cart])
 
   return (
     <div className="App">
