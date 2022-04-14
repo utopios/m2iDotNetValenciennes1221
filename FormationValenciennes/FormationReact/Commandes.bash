b65e2a8e329cd998a150ab77a367b730307ab1c9
PetitVulcan <dipersio.a@gmail.com>
2022-04-14 14:02:39 +0200

    Add FontAwesome Instructions

--- FormationValenciennes/FormationReact/Commandes.bash
+++ FormationValenciennes/FormationReact/Commandes.bash
@@ -1,10 +1,14 @@
 ## Install React project from CLI NPX
 npx create-react-app@latest <projectName>
 
-## Ajout dépendance projet React
-npm install --save-dev web-vitals
+## Procedure (New)
+1/ npx create-react-app <nomProjet>
 
-## Procedure
+2/ cd <nomProjet>
+
+3/ npm start
+
+## Procedure (Old)
 1/ npx create-react-app <nomProjet>
 
 2/ cd <nomProjet>
@@ -32,3 +36,31 @@ $ npm install react-bootstrap bootstrap@5.1.3
 
 ## The following line can be included in your src/index.js or App.js file
 import 'bootstrap/dist/css/bootstrap.min.css';
+
+####
+## Install Fontawesome
+####
+
+# Installer tous en une seule fois
+npm i -S @fortawesome/fontawesome-svg-core @fortawesome/react-fontawesome @fortawesome/free-regular-svg-icons @fortawesome/free-solid-svg-icons @fortawesome/free-brands-svg-icons
+
+# Exécutez la commande suivante pour installer les paquets de base :
+npm i -S @fortawesome/fontawesome-svg-core @fortawesome/react-fontawesome
+# regular icons
+npm i -S @fortawesome/free-regular-svg-icons
+# solid icons
+npm i -S @fortawesome/free-solid-svg-icons
+# brand icons
+npm i -S @fortawesome/free-brands-svg-icons
+
+## Import Fontawesome
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+# Import solid 
+import { faHome } from "@fortawesome/free-solid-svg-icons";
+# Import Regular
+import { faHome } from "@fortawesome/free-regular-svg-icons";
+# Import brand
+import { faHome } from "@fortawesome/free-brand-svg-icons";
+
+## Use Font Awesome
+<FontAwesomeIcon icon={faHome} style={{ color: 'rgb(40, 44, 52)',fontSize:'25px' }} />
